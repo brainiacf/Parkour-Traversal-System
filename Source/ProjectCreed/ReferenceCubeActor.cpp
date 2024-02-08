f440e2f5a15e9b28e80970cec1579b84975085af
nikunj <70919626+brainiacf@users.noreply.github.com>
2024-02-08 10:25:02 +0530

    basic Cube Reference actor

--- Source/ProjectCreed/ReferenceCubeActor.cpp
+++ Source/ProjectCreed/ReferenceCubeActor.cpp
@@ -15,18 +15,23 @@ AReferenceCubeActor::AReferenceCubeActor()
 	// RED ARROW
 	ArrowComponentRed = CreateDefaultSubobject<UArrowComponent>(TEXT("ArrowComponentRed"));
 	ArrowComponentRed->ArrowColor = FColor::Red;
+	ArrowComponentRed->ArrowSize = 10.0f;
 	ArrowComponentRed->bHiddenInGame = false;
 	ArrowComponentRed->SetupAttachment(CubeMesh);
 	// GREEN ARROW
 	ArrowComponentGreen = CreateDefaultSubobject<UArrowComponent>(TEXT("ArrowComponentGreen"));
 	ArrowComponentGreen->ArrowColor = FColor::Green;
+	ArrowComponentGreen->ArrowSize = 10.0f;
 	ArrowComponentGreen->bHiddenInGame = false;
 	ArrowComponentGreen->SetupAttachment(CubeMesh);
+	ArrowComponentGreen->SetWorldRotation(FRotator(0.0f,90.0f,0.0f));
 	// BLUE ARROW
 	ArrowComponentBlue = CreateDefaultSubobject<UArrowComponent>(TEXT("ArrowComponentBlue"));
 	ArrowComponentBlue->ArrowColor = FColor::Blue;
+	ArrowComponentBlue->ArrowSize = 10.0f;
 	ArrowComponentBlue->bHiddenInGame = false;
 	ArrowComponentBlue->SetupAttachment(CubeMesh);
+	ArrowComponentBlue->SetWorldRotation(FRotator(90.0f,0.0f,0.0f));
 	
 
 
